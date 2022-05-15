64a91737e4611bdd1947dbefa36b3fc0cdefd016
filobr <filip.obracaj@gmail.com>
2022-05-15 14:49:59 +0200

    feat: siteContent component made to handle inout arrays of various sizes
    Influencers and Campaign components made to check siteContent working

--- src/components/siteContent/SiteContent.js
+++ src/components/siteContent/SiteContent.js
@@ -2,31 +2,20 @@ import Modal from "components/modal/Modal";
 import { useState } from "react";
 import styles from "components/siteContent/siteContent.module.css";
 import PropTypes from "prop-types";
-const SiteContent = ({ group1, group2, group3 }) => {
+const SiteContent = ({ groups }) => {
   const [selectedGroup, setSelectedGroup] = useState(null);
   const [selectedPhotoIndex, setSelectedPhotoIndex] = useState(null);
   const [isModalOpened, setIsModalOpened] = useState(false);
   const [isDescriptionOpened, setIsDescriptionOpened] = useState(false);
   const [isPhotoHovered, setIsPhotoHovered] = useState(null);
 
-  const mainPhotos = [group1[0], group2[0], group3[0]];
+  const mainPhotos = groups.map(group => group.photos[0]);
 
   const onPhotoClick = ({ currentTarget }) => {
-    switch (currentTarget.dataset.index) {
-      case "0":
-        setSelectedGroup(group1);
-        break;
-      case "1":
-        setSelectedGroup(group2);
-        break;
-      case "2":
-        setSelectedGroup(group3);
-        break;
-      default:
-        break;
-    }
+    setSelectedGroup(groups[currentTarget.dataset.index]);
     setSelectedPhotoIndex(0);
     setIsModalOpened(true);
+    return;
   };
 
   const onPhotoHover = ({ currentTarget }) => {
@@ -59,7 +48,12 @@ const SiteContent = ({ group1, group2, group3 }) => {
                 onMouseLeave={onMouseLeave}
               />
               {isDescriptionOpened && index === Number(isPhotoHovered) && (
-                <div className={styles.description}>{isPhotoHovered}</div>
+                <div className={styles.description}>
+                  <p style={{ fontWeight: "bold" }}>
+                    {groups[isPhotoHovered].description}
+                  </p>
+                  <p>Click me to see more content from this session</p>
+                </div>
               )}
             </div>
           );
@@ -68,7 +62,7 @@ const SiteContent = ({ group1, group2, group3 }) => {
       {isModalOpened && (
         <Modal
           selected={selectedPhotoIndex}
-          photos={selectedGroup}
+          photos={selectedGroup.photos}
           setSelected={setSelectedPhotoIndex}
           setIsModalOpened={setIsModalOpened}
         />
@@ -78,9 +72,9 @@ const SiteContent = ({ group1, group2, group3 }) => {
 };
 
 SiteContent.propTypes = {
-  group1: PropTypes.arrayOf(PropTypes.string),
-  group2: PropTypes.arrayOf(PropTypes.string),
-  group3: PropTypes.arrayOf(PropTypes.string),
+  groups: PropTypes.arrayOf(
+    PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string), PropTypes.string)
+  ),
 };
 
 export default SiteContent;
--- src/views/fashionStyling/Influencers.js
+++ src/views/fashionStyling/Influencers.js
@@ -2,7 +2,7 @@ import importAll from "components/ImportAll";
 import SiteContent from "components/siteContent/SiteContent";
 import { useEffect } from "react";
 
-const fagata = importAll(
+const fagataImg = importAll(
   require.context(
     "assets/fashionStyling/influencers/@fagataaa - Agata Fąk",
     false,
@@ -10,7 +10,7 @@ const fagata = importAll(
   )
 );
 
-const natsu = importAll(
+const natsuImg = importAll(
   require.context(
     "assets/fashionStyling/influencers/NATSU - Natalia Karczmarczyk",
     false,
@@ -18,7 +18,7 @@ const natsu = importAll(
   )
 );
 
-const zalewska = importAll(
+const zalewskaImg = importAll(
   require.context(
     "assets/fashionStyling/influencers/@melamodelayt -  Amelia Zalewska",
     false,
@@ -26,12 +26,29 @@ const zalewska = importAll(
   )
 );
 
+const fagata = {
+  photos: fagataImg,
+  description: "@fagataaa - Agata Fąk",
+};
+
+const natsu = {
+  photos: natsuImg,
+  description: "NATSU - Natalia Karczmarczyk",
+};
+
+const zalewska = {
+  photos: zalewskaImg,
+  description: "@melamodelayt - Amelia Zalewska",
+};
+
+const groups = [fagata, natsu, zalewska];
+
 const Influencers = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  return <SiteContent group1={fagata} group2={natsu} group3={zalewska} />;
+  return <SiteContent groups={groups} />;
 };
 
 export default Influencers;
