7c845007903c40511a64bfe17d372eac7bbfa5db
filobr <filip.obracaj@gmail.com>
2022-07-11 18:12:14 +0200

    Fix: Controls on mobile added and autoplay disabled

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import styles from "components/video/video.module.css";
 import poster from "assets/newCollection/homeOffice/LookBook/5. busy/1.jpg";
+import { useEffect } from "react";
 
 const Video = ({ videoSrc }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(!(document.documentElement.clientWidth > 640));
+  }, []);
+
   return (
     <div className={styles.videoContainer}>
-      <video className={styles.video} poster={poster} autoPlay loop muted>
-        <source src={videoSrc} type="video/mp4" />
-      </video>
+      {isMobile ? (
+        <video className={styles.video} poster={poster} controls>
+          <source src={videoSrc} type="video/mp4" />
+        </video>
+      ) : (
+        <video className={styles.video} poster={poster} autoPlay loop muted>
+          <source src={videoSrc} type="video/mp4" />
+        </video>
+      )}
     </div>
   );
 };
